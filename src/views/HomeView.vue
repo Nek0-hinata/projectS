f92e2ba56c20707e3ec571a9b5fef42806a7545b
Nek0-hinata <tony1160578676@gmail.com>
2024-03-20 14:36:40 +0800

    feat(all): switch from vue to nextjs
    BREAKING CHANGE: I decide to switch from vue to nextjs, that will be more compatible with my tech
    stack

--- src/views/HomeView.vue
+++
@@ -1,8 +0,0 @@
-<script lang="ts" setup>
-import KnowledgeGraph from '@/components/knowledgeGraph/knowledgeGraph.vue'
-</script>
-
-<template>
-  <!--    <TheWelcome />-->
-  <KnowledgeGraph />
-</template>
